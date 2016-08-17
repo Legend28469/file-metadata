f93dc493cca6a423617d9ccb25cd57f2ce006732
Johnathan Brown <johnathaneb@gmail.com>
2016-08-17 19:12:24 -0400

    Added upload and JSON return functionality

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var path = require("path");
+var fs = require("fs");
+
+var multer = require("multer");
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
+
+app.post('/upload', upload.single('file'), function (req, res) {
+    fs.unlink(req.file.path);
+
+    res.json({
+        size: req.file.size
+    })
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
